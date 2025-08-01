1b57e929b654fc961b15a5aa887c22c9e6dc693e
Kamaria932 <83671576+Kamaria932@users.noreply.github.com>
2025-08-01 17:06:35 +0000

    Working on Part 1: Think through

--- RecursiveExercises.java
+++ RecursiveExercises.java
@@ -1,1 +1,17 @@
 //My Exercises for Part 1-4 of this assingment will go here.
+import Arrays;
+/* Part 1: 
+ * Goal: Find the largest element within the array using recursive methods.
+ * Subarray has one element this element is the larget
+ * Hint: Similar to how merge sort divides arrays.
+ * Split the array in half, find the largest element from each division, and then compare the larger values. Return the largest element.
+*/
+
+public class recursiveExercises {
+    public static int findMax(int[] arr, int left, int right) {
+        if (left == right) { //If the subarray has one element.
+            return arr[left]; //retunr the one elemenet. This is the largest number.
+        }
+
+    }
+}
