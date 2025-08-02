247dbad787925989797d22c0b8485506c0572615
Kamaria932 <83671576+Kamaria932@users.noreply.github.com>
2025-08-02 16:45:03 +0000

    Finalizing Problem 1

--- RecursiveExercises.java
+++ RecursiveExercises.java
@@ -10,8 +10,16 @@ import Arrays;
 public class recursiveExercises {
     public static int findMax(int[] arr, int left, int right) {
         if (left == right) { //If the subarray has one element.
-            return arr[left]; //retunr the one elemenet. This is the largest number.
+            return arr[left]; //Return the one elemenet. This is the largest number.
         }
-
-    }
-}
+        int mid = (left + right) / 2; //Divide the array into two smaller arrays.
+        int maxLeft = findMax (arr, left, mid); //Find the max value of the left subarray.
+        int maxRight = findMax (arr, mid + 1, right); //Find the max value of the right subarray.
+        /*Compare the large values in each subarray and return the largest. */
+        if (maxLeft > maxRight) {
+            return maxLeft;
+        }else {
+            return maxRight;
+        }
+        }
+    } //end of class
